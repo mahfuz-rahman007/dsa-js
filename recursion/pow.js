bd2e6b4a2477e2a52d9971fed55c2cc671aa89f9
mahfuz-rahman007 <mahfujkhan017@gmail.com>
2024-07-06 18:58:42 +0600

    pow recursion

---
+++ recursion/pow.js
@@ -0,0 +1,16 @@
+
+/**
+ * Return Power By using Recursion
+ * 
+ */
+function power(base, exp){
+
+    if(exp == 0) return 1;
+
+    return base * power(base, exp-1);
+    
+}
+
+console.log(power(2,0)) // 1
+console.log(power(2,2)) // 4
+console.log(power(2,4)) // 16

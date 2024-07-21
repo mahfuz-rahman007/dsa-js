e9f72b4532315790380a7bd575d134a2254ac14e
mahfuz-rahman007 <mahfujkhan017@gmail.com>
2024-07-21 10:45:02 +0600

    tree insert method

--- tree/practice.js
+++ tree/practice.js
@@ -10,8 +10,46 @@ class BinarySearchTree {
     constructor() {
         this.root = null;
     }
+
+    insert(value) {
+        let new_node = new Node(value);
+        if(this.root === null) {
+            this.root = new_node;
+            return true
+        }
+
+        let temp = this.root;
+
+        while(temp) {
+            if(temp.value === value) return false;
+
+            if(value < temp.value) {
+
+                if(temp.left === null) {
+                    temp.left = new_node;
+                    return true;
+                }
+                temp = temp.left;
+            } else {
+
+                if(temp.right === null) {
+                    temp.right = new_node;
+                    return true;
+                }
+                temp = temp.right;
+            }
+
+        }
+
+    }
 }
 
 let tree = new BinarySearchTree();
+
+tree.insert(40);
+tree.insert(20);
+tree.insert(60);
+tree.insert(25);
+tree.insert(19);
 
 console.log(tree.root);

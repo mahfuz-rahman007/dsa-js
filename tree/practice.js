158ea70ff6bc661d278038fc6b0cbbc41ca733cc
mahfuz-rahman007 <mahfujkhan017@gmail.com>
2024-07-21 10:23:33 +0600

    tree construcor

---
+++ tree/practice.js
@@ -0,0 +1,17 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+}
+
+let tree = new BinarySearchTree();
+
+console.log(tree.root);

55952f4a6f835a29f9249a4d86b443fa897fa1c6
mahfuz-rahman007 <mahfujkhan017@gmail.com>
2024-07-22 11:15:19 +0600

    map practice

---
+++ hash-map/practice.js
@@ -0,0 +1,11 @@
+let map = new Map();
+
+let p = map.set(23, "Helpp");
+map.set(true, "movie");
+
+console.log(map);
+console.log(map.get(23));
+console.log(map.values());
+console.log(map.entries());
+console.log(map.keys());
+console.log(map.size);

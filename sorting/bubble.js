310fec468ac6fc7ad6f2b72e8055fc9731a47ce4
mahfuz-rahman007 <mahfujkhan017@gmail.com>
2024-07-22 17:12:24 +0600

    bubble sort

---
+++ sorting/bubble.js
@@ -0,0 +1,22 @@
+
+
+function bubble_sort(array) {
+    let c = 0;
+    for (let i = 0; i < array.length; i++) {
+        c++;
+        for (let j = 0; j < array.length - i; j++) {
+            c++;
+            if(array[j] > array[j+1]) {
+                let temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+            }   
+        }
+        
+    }
+
+    return array;
+}
+
+console.log(bubble_sort([1,3,7,4,5,9,2])); // [1,2,3,4,5,7,9]
+console.log(bubble_sort([9,7,5,4,3,2,1,6])); // [1,2,3,4,5,7,9]
